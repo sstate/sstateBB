61e51991e7ceeb35fbbce3b26d71818bc7999d53
Richard Torruellas <rtorruellas@gmail.com>
2015-04-01 15:13:22 -0700

    adding twitter auth

---
+++ collections/admins.js
@@ -0,0 +1,17 @@
+'use strict';
+var _ = require('lodash');
+
+var db = require('./../lib/db');
+
+var Admin = db.Model.extend({
+  tableName: 'bb_admins',
+  hasTimestamps: true
+});
+
+var Admins = db.Collection.extend({
+  model: Admin
+});
+
+var admins = new Admins();
+
+module.exports = {Collection: admins, Model: Admin};
